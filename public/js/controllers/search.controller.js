7807aca80a8ae3952169301e0b141a1238488ad1
John Sweriduk <john@stackandsizzle.com>
2020-06-21 07:24:24 -0700

    basic updates to stock_controller.js, search.controller.js, updated package.json and package-lock.json to include the request middleware

--- public/js/controllers/search.controller.js
+++ public/js/controllers/search.controller.js
@@ -1,4 +1,18 @@
 // Handles the views for stocks
-app.controller('SearchController', function($scope) {
-    console.log('test');
+app.controller('SearchController', function($scope, $http) {
+    this.stocks = [];
+    this.testCall = () => {
+        $http({
+            method: 'GET',
+            url: '/admin/stock/aggressive'
+        }).then(
+            response => {
+                console.log(response.data);
+                this.stocks = response.data;
+            },
+            error => {
+                console.log(error);
+            }
+        )
+    };
 });
