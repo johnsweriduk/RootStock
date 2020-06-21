c9592669c6866758310dcbdea70e50ab0f39f834
John Sweriduk <john@stackandsizzle.com>
2020-06-20 17:41:37 -0700

    model is now updated

--- models/portfolio.js
+++ models/portfolio.js
@@ -1,9 +1,12 @@
 const mongoose = require(`mongoose`);
 
 const portfolioSchema = new mongoose.Schema({
-    conservative: [ String ],
-    moderate: [ String ],
-    aggressive: [ String ],
+    // contains an array of objects.
+    // the string represents the stock ticker, for api requests
+    // the number is the number of shares owned in that stock
+    conservative: [ {ticker: String, shares: Number} ],
+    moderate: [ {ticker: String, shares: Number} ],
+    aggressive: [ {ticker: String, shares: Number} ],
     conservativePercent: { type: Number },
     moderatePercent: { type: Number },
     aggressivePercent: { type: Number },
