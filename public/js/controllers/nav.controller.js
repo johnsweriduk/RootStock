837ef67c57c539a6d93f96723069466bd0c4e0a6
John Sweriduk <john@stackandsizzle.com>
2020-06-23 09:27:56 -0700

    created logout functionality. automatically redirect to /portfolio on login

--- public/js/controllers/nav.controller.js
+++ public/js/controllers/nav.controller.js
@@ -1,6 +1,18 @@
 // Handles the views for stocks
-app.controller('NavController', function() {
+app.controller('NavController', ['$http','$scope', '$location', function($http, $scope, $location) {
     this.logout = () => {
-        
+        console.log('test');
+        $http({
+            method: 'DELETE',
+            url: '/admin/session'
+        }).then(
+            response => {
+                console.log('test');
+                console.log(response);
+                $location.path("/");
+            }, error => {
+
+            }
+        );
     }
-});
+}]);
